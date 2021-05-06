05ff258fe572d7d600f6c8d08df9687f89ad7ec9
Adam Bolton <adam_bolton90@hotmail.com>
2021-05-06 23:28:34 +0900

    added search function file

--- search-function.js
+++ search-function.js
@@ -1,1 +1,10 @@
-console.log('hello');
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    Object.values(allCards).filter((card) => {
+        if(card.id.toLowerCase().includes(searchString)) {
+            card.style.display = "";
+        } else {
+            card.style.display = "none";
+        }
+    })
+});
